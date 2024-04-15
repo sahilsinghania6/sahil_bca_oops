a6da1a703c652dd82448f50de87c43906dd6b935
sahilsinghania6 <162978490+sahilsinghania6@users.noreply.github.com>
2024-04-15 21:37:13 +0530

    Update ans 3.cpp

--- ans 3.cpp
+++ ans 3.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 class Test {
 public:
-  // Default constructor initializes all members to 0
+  
   Test() {
     num1 = num2 = num3 = 0;
   }
 
-  // get_data() function takes variable number of arguments
+ 
   void get_data(int arg1, int arg2 = 0, int arg3 = 0) {
     num1 = arg1;
     num2 = arg2;
     num3 = arg3;
   }
 
-  // show_data() function displays the values of member variables
+ 
   void show_data() {
     std::cout << "num1: " << num1 << ", num2: " << num2 << ", num3: " << num3 << std::endl;
   }
@@ -26,15 +26,15 @@ private:
 };
 
 int main() {
-  // Create three objects of the Test class
+ 
   Test obj1, obj2, obj3;
 
-  // Invoke get_data() member function with different arguments
+ 
   obj1.get_data(10);
   obj2.get_data(20, 30);
   obj3.get_data(40, 50, 60);
 
-  // Display the values of all the objects
+ 
   std::cout << "Object 1:" << std::endl;
   obj1.show_data();
 
